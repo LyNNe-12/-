7cfc67558d3a85948b15885a86f446b401f5b6a3
LyNNe-12 <67426332+LyNNe-12@users.noreply.github.com>
2025-01-21 19:22:26 +0300

    ebu babul`ku

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ let shell = document.querySelector('.shell');
 let btn = document.querySelector('.btn');
 let image = document.querySelector('.image');
 let b_image = document.querySelector('.b_image');
-btn.onclick = function() {
+btn.addEventListener('click', () => {
 	shell.style.display = 'none';
 	btn.style.display = 'none';
 	image.style.display = 'inline-block';
 	b_image.style.height = '100vh';
-}
+});
